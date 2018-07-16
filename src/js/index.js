f362d658cdadba3c7814c35bb7771f745fe82370
luisfelipedesouza <luis.felpe.souza@gmail.com>
2018-07-16 07:58:21 -0300

    List model

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,9 @@
 import Search from './model/Search';
 import Recipe from './model/Recipe';
+import List from './model/List';
 import * as searchView from './view/searchView';
 import * as recipeView from './view/recipeView';
+import * as listView from './view/listView';
 import {elements, renderLoader, clearLoader} from './view/base';
 
 /**
@@ -81,6 +83,29 @@ const controlRecipe = async () => {
 };
 ['hashchange', 'load'].forEach(event => window.addEventListener(event, controlRecipe));
 
+/**
+ * LIST CONTROLLER
+ */
+const controlList = () => {
+    if(!state.list) state.list = new List();
+
+    state.recipe.ingredients.forEach(el => {
+        const item = state.list.addItem(el.count, el.unit, el.ingredient);
+        listView.renderItem(item)
+    });
+};
+
+elements.shopping.addEventListener('click', e => {
+    const id = e.target.closest('.shopping__item').dataset.itemid;
+    if(e.target.matches('.shopping__delete, .shopping__delete *')) {
+        state.list.deleteItem(id);
+        listView.deleteItem(id);
+    } else if(e.target.matches('.shopping__count-value')) {
+        const val = parseFloat(e.target.value, 10);
+        state.list.updateCount(id, val);
+    }
+});
+
 elements.recipe.addEventListener('click', e => {
     if(e.target.matches('.btn-decrease, .btn-decrease *')) {
         if(state.recipe.servings > 1) {
@@ -90,5 +115,7 @@ elements.recipe.addEventListener('click', e => {
     } else if(e.target.matches('.btn-increase, .btn-increase *')) {
         state.recipe.updateServings('inc');
         recipeView.updateServingsIngredients(state.recipe);
+    } else if(e.target.matches('.recipe__btn--add, .recipe__btn--add *')) {
+        controlList();
     }
 });
